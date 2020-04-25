26dc520179e069d2e7adaf5a8879e2b4e5f8d1a0
kubotak0630 <kubotak@f01.itscom.net>
2020-04-25 22:12:05 +0900

    Implement loadingIcon

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
 
+		
     <div>Hello Vue.js</div>
     <p>{{flg}}</p>
     <el-button type="primary" @click="handleClick">Button</el-button>
 		<el-button type="primary" @click="onButtonChangeData">ChageData</el-button>
-    <srcp-code-show v-bind:table-data="tableData"></srcp-code-show>
-
+    <i class="el-icon-loading" v-show="loadingFlg"></i>
+		<srcp-code-show v-bind:table-data="tableData"></srcp-code-show>
+		
+		
+		<el-backtop :visibility-height="2000"></el-backtop>
+		
+		
   </div>
 </template>
 
@@ -25,7 +31,8 @@ export default {
       flg: true,
       rcp0: [{pc: 0, name: 'CSA_HOGEHOGE_00', srcp:['0001','0002','0003']},{pc: 1, name: 'CSA_HOGEHOGE_01', srcp:['1001','1002','1003']},{pc: 2, name: 'CSA_HOGEHOGE_02', srcp:['2001','2002','2003']}],
       rcp1: [{pc: 0, name: 'CSA_HOGEHOGE_10', srcp:['000000000000000000000000000000000000000000000000011','0022','0023','0024']},{pc: 2, name: 'CSA_HOGEHOGE_11', srcp:['1011','1022','1033']},{pc: 4, name: 'CSA_HOGEHOGE_12', srcp:['2011','2022','2033']}],
-      tableData: []
+			tableData: [],
+			loadingFlg: false
      
     }
   },
@@ -41,18 +48,26 @@ export default {
       }
     },
     onButtonChangeData: function() {
-      this.tableData = [];
-      for (let i = 0; i < 200; i++) {
-        const tempString0 = Math.random().toString(32).substring(2);
-        const tempString1 = Math.random().toString(32).substring(2);
-        const tempString2 = Math.random().toString(32).substring(2);
-        const tempString3 = Math.random().toString(32).substring(2);
-        const tempString4 = Math.random().toString(32).substring(2);  
-        this.tableData.push({pc:i, name: tempString0, msg1: tempString1, msg2: tempString2, msg3: tempString3})
-      }
+			this.loadingFlg = true;
+
+			setTimeout( ()=> {
+				this.tableData = [];
+      	for (let i = 0; i < 4000; i++) {
+        	const tempString0 = Math.random().toString(32).substring(2);
+        	const tempString1 = Math.random().toString(32).substring(2);
+        	const tempString2 = Math.random().toString(32).substring(2);
+        	const tempString3 = Math.random().toString(32).substring(2);
+        	const tempString4 = Math.random().toString(32).substring(2);  
+					this.tableData.push({pc:i, name: tempString0, msg1: tempString1, msg2: tempString2, msg3: tempString3})
+					this.loadingFlg = false;
+      	}
+			}, 50);
+      
       // console.log(this.tableData)
       console.log('tableData Created')
-    }
+		},
+		
+		
   }
 }
 </script>
