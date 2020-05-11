4dd519f03c8d4c9d507cc9bc2c38ed90cf25ee65
kubotak0630 <kubotak@f01.itscom.net>
2020-05-11 21:05:54 +0900

    change vue-style-loader->style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
       //SCSS
       {
         test: /\.scss$/,
-        use: ["vue-style-loader", "css-loader", "sass-loader"],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       //url-loader
       {
