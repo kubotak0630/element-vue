28f8a6b4c14ab1a44029b2293e74a30642c9022f
kubotak0630 <kubotak@f01.itscom.net>
2020-05-18 22:17:41 +0900

    add filedrop

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,15 @@
   <div id="app">
     <div>Hello Vue.js</div>
     <p>{{ flg }}</p>
+    <div class="dump-file-open">
+      <FileDropBox
+        v-on:emit-freaddone-event="OnFileRead"
+        v-bind:isEnable="!checked"
+        id_str="dumpfile-cmp"
+        text_label="Dumpファイルを選択 or FileDrop"
+      ></FileDropBox>
+      <el-checkbox v-model="checked">fileを選択しない</el-checkbox>
+    </div>
     <el-button type="primary" @click="handleClick">Button</el-button>
     <el-button type="primary" @click="onButtonChangeData">ChageData</el-button>
     <i class="el-icon-loading" v-show="loadingFlg"></i>
@@ -12,25 +21,31 @@
 </template>
 
 <script>
-import SrcpCodeShow from "./components/SrcpCodeShow.vue";
+import SrcpCodeShow from './components/SrcpCodeShow.vue';
+import FileDropBox from './components/FileDropBox.vue';
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
     SrcpCodeShow,
+    FileDropBox
   },
   data: function() {
     return {
-      message: "hello Vue.js!!",
+      message: 'hello Vue.js!!',
       flg: true,
       tableData: [],
       loadingFlg: false,
+      checked: false
     };
   },
   methods: {
     handleClick: function(event) {
       console.log(event);
       this.flg = !this.flg;
+    },
+    OnFileRead: function() {
+      console.log('Call OnFileRead');
     },
     onButtonChangeData: function() {
       this.loadingFlg = true;
@@ -55,17 +70,21 @@ export default {
             name: tempString0,
             msg1: tempString1,
             msg2: tempString2,
-            msg3: tempString3,
+            msg3: tempString3
           });
           this.loadingFlg = false;
         }
       }, 50);
 
       // console.log(this.tableData)
-      console.log("tableData Created");
-    },
-  },
+      console.log('tableData Created');
+    }
+  }
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.dump-file-open {
+  margin-bottom: 40px;
+}
+</style>
